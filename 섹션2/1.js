c7da2ff3f7fe059a0df3b6c65d3c1dfb10fd9564
ChoongLim <bcl0206@naver.com>
2024-01-11 09:56:40 +0900

    feat: 2-1 풀이

--- 섹션2/1.js
+++ 섹션2/1.js
@@ -1,9 +1,27 @@
-function myAnswer(n, ...rest){
-                const answer = rest.filter((i) => (i >= n))
+function myAnswer(arr){
+            let answer;
+
+            answer = arr.filter((_, idx)=> {
+                if(idx === 0) return true
+                else if(arr[idx-1] < arr[idx]) return true
+            })
+
                 return answer;
             }
 
-console.log((myAnswer(6,7,3,9,5,6,12)));
+console.log((myAnswer([7,3,9,5,6,12])));
 
 
+// ==================================================
+function solution (arr){
+    let answer = [];
 
+    answer.push(arr[0]);
+    for(let i = 1 ; i < arr.length ; i++){
+        if(arr[i] > arr[i-1]) answer.push(arr[i]);
+    }
+
+    return answer
+}
+
+console.log((solution([7,3,9,5,6,12])));
