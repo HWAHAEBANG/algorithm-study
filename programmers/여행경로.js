73ef4fa5ad5fa97f6d2a5381c9c755bc6ab3794a
ChoongLim <bcl0206@naver.com>
2024-03-01 22:57:12 +0900

    feat: 여행경로

---
+++ programmers/여행경로.js
@@ -0,0 +1,9 @@
+// 미결
+
+function solution(tickets) {
+    var answer = [];
+    return answer;
+}
+
+console.log(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]));
+console.log(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]));
