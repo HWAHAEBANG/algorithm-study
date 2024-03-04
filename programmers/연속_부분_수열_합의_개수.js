76ae3d1b9a9709c267d852b0bc424c97ed2f8b66
ChoongLim <bcl0206@naver.com>
2024-03-04 16:40:56 +0900

    feat: 연속 부분 수열 합의 개수

---
+++ programmers/연속_부분_수열_합의_개수.js
@@ -0,0 +1,14 @@
+function solution(arr){
+    let tmp = []
+    let n = arr.length
+
+    // for(let i=0; i<arr.length; i++){
+    //     for(let j=0; j<arr.length; j++){
+    //         tmp.push(arr[i] + arr[j])
+    //     }
+    // }
+
+    console.log(tmp);
+}
+
+console.log(solution([7,9,1,1,4]));
