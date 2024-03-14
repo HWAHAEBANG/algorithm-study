435a51fc530ff9aa7402d7e91c7b9a889978251b
ChoongLim <bcl0206@naver.com>
2024-03-14 10:16:23 +0900

    feat: 아나그램 판별

--- 선순위빈출/8.아나그램_판별.js
+++ 선순위빈출/8.아나그램_판별.js
@@ -18,3 +18,24 @@ function anagram(a,b){
 
 console.log(anagram('AbaAeCe', 'baeeACA'));
 console.log(anagram('abaCC', 'Caaab'));
+
+
+function practice1(a, b){
+    let answer = 'YES'
+    let sH = new Map();
+
+    for(let x of a){
+        if(sH.has(x)) sH.set(x, sH.get(x) + 1);
+        else sH.set(x, 1)
+    }
+
+    for(let x of b){
+        if(!sH.has(x) || sH.get(x) === 0) answer = 'NO' 
+        else sH.set(x, sH.get(x) - 1);
+    }
+
+    return answer
+}
+
+console.log(practice1('AbaAeCe', 'baeeACA'));
+console.log(practice1('abaCC', 'Caaab'));
