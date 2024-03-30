da576b3f76bcfe5bbfd584c86a7396757de5594b
ChoongLim <bcl0206@naver.com>
2024-03-30 09:16:04 +0900

    feat: 팩토리얼 구현

--- 선순위빈출/4.팩토리얼_구현.js
+++ 선순위빈출/4.팩토리얼_구현.js
@@ -10,3 +10,14 @@ function factorial(n){
 
 console.log(factorial(5));
 
+function practice1(n){
+    let answer = 1
+
+    for(let i=2; i<=n; i++){
+        answer *= i
+    }
+
+    return answer
+}
+
+console.log(practice1(5));
