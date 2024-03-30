2cc81408b325a06f0c63f37c0786aa8ceab04f0d
ChoongLim <bcl0206@naver.com>
2024-03-30 09:16:59 +0900

    feat: 1부터 n까지 더하기

--- 선순위빈출/11.1부터_n까지_더하기.js
+++ 선순위빈출/11.1부터_n까지_더하기.js
@@ -24,3 +24,16 @@ function practice2(n){
 }
 
 console.log(practice2(5));
+
+// ★★★★★★★★★★★★// ★★★★★★★★★★★★// ★★★★★★★★★★★★
+// 좋지 않은 접근법이었다. // 메모이제이션 아니면 이거 웬만하면 쓰지말자..
+// function practice3(n, sum){
+//     sum = sum || 0
+
+//     return n === 1 ? n : sum + practice(n-1, sum)
+// }
+// console.log(practice3(5));
+
+function practice3(n){
+    return n === 1 ? n : n + practice3(n-1);
+}
