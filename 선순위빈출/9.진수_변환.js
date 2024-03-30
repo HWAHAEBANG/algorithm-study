d62c76056c23fff0bbf253412a05d3a27b5aaacc
ChoongLim <bcl0206@naver.com>
2024-03-30 09:16:45 +0900

    feat: 진수 변환

--- 선순위빈출/9.진수_변환.js
+++ 선순위빈출/9.진수_변환.js
@@ -90,3 +90,59 @@ function practiceDecimal(n){
 }
 
 console.log('연습십진수',practiceDecimal('1010'));
+
+//==========
+
+// 10진수 => 2진수
+console.log('10진수 => 2진수', dec.toString(2));
+
+// 10진수 => 8진수
+console.log('10진수 => 8진수', dec.toString(8));
+
+// 10진수 => 16진수
+console.log('10진수 => 16진수', dec.toString(16));
+
+// 16 진수 => 2진수 (10진수로 바꿨다가 2진수로)
+console.log('16진수 => 2진수', parseInt(hex, 16).toString(2));
+
+// 16진수 => 10진수
+console.log('16진수 => 10진수',parseInt(hex, 16)); // 123
+
+// 2진수 10진수
+console.log('2진수 => 10진수', parseInt(bin, 2));
+
+// 2진수 16진수 (10주로 바꿨다가 16진수로)
+console.log('2진수 => 16진수', parseInt(bin, 2).toString(16));
+
+// 이진수 변환기
+function practiceBinary1(n, answer){
+    answer = answer || ''
+    if(n <= 1) return n + answer 
+    else {
+        answer = n%2 + answer
+        return practiceBinary1(parseInt(n/2), answer)
+    }
+}
+
+console.log('연습이진수1', practiceBinary1(10));
+
+// 이게 되네 한줄로......
+function practiceBinary11(n){
+   return n <= 1 ? `${n}` : practiceBinary11(parseInt(n/2)) + n%2 
+}
+
+console.log('연습이진수11', practiceBinary11(10));
+
+
+// 십진수 변환기
+function practiceDecimal1(n){
+    let decimal = 0
+    
+    for(let i=0; i<n.length; i++){
+        decimal += n[n.length - i -1] * 2**i
+    }
+
+    return decimal
+}
+
+console.log('연습십진수',practiceDecimal('1010'));
