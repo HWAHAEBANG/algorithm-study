d88d5d4d7e4e8051610f374545cbd68c4fb9fb10
ChoongLim <bcl0206@naver.com>
2024-04-04 14:40:19 +0900

    feat: 최대 공약수

--- 선순위빈출/7.최대_공약수.js
+++ 선순위빈출/7.최대_공약수.js
@@ -30,3 +30,15 @@ function practice2(a, b){
 }
 
 console.log(practice2(24,24));
+
+function practice3(a,b){
+    let remain = a % b
+
+    // if(remain === 0) return b;
+
+    // return practice3(b, remain)
+
+    return remain === 0 ? b : practice3(b, remain)
+}
+
+console.log(practice3(24,24));
