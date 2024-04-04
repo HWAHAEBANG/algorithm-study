fb4ff18e69a8b4614eae1d6d38b0358df0b5c945
ChoongLim <bcl0206@naver.com>
2024-04-04 14:40:26 +0900

    feat: 아나그램 판별

--- 선순위빈출/8.아나그램_판별.js
+++ 선순위빈출/8.아나그램_판별.js
@@ -60,3 +60,24 @@ function practice2(a,b){
 
 console.log(practice2('AbaAeCe', 'baeeACA'));
 console.log(practice2('abaCC', 'Caaab'));
+
+
+function practice3(a,b){
+    let answer = 'yes';
+    let sH = new Map()
+    
+    for(let x of a){
+        if(sH.has(x)) sH.set(x, sH.get(x)+1)
+        else sH.set(x, 1)
+    }
+
+    for(let x of b){
+        if(!sH.has(x) || sH.get(x) <= 0) return answer = 'no' // 여기서 그냥 바로 리턴해버리면 불필요한 반복 줄일 수 있겠네
+        else sH.set(x, sH.get(x)-1)  
+    }
+
+    return answer
+}
+
+console.log('하이',practice3('AbaAeCe', 'baeeACA'));
+console.log('하이',practice3('abaCC', 'Caaab'));
